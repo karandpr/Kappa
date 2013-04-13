cfd805c6a2b8a7fa97b60d9ea3415cfdf641f7fc
KaSt <k@ka.st>
2013-04-13 19:11:04 +0200

    Rebased LMK, Removed Fudgeswap

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -34,24 +34,22 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
+#include <linux/rcupdate.h>
 #include <linux/notifier.h>
-
-#ifdef CONFIG_SWAP
-#include <linux/fs.h>
 #include <linux/swap.h>
-#endif
+
+#define SEC_ADJUST_LMK
 
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES
 #include <linux/kobject.h>
 #include <linux/string.h>
 #include <linux/sysfs.h>
 
-static int auto_detect = 1;
-static int init_kobject(void);
+static int auto_detect = 0; //Autodetect disabled by Default.
 
+static int init_kobject(void);
 #endif
 
-#define DEBUG_LEVEL_DEATHPENDING 6
 
 static uint32_t lowmem_debug_level = 1;
 static int lowmem_adj[6] = {
@@ -61,7 +59,7 @@ static int lowmem_adj[6] = {
 	12,
 };
 static int lowmem_adj_size = 4;
-static size_t lowmem_minfree[6] = {
+static int lowmem_minfree[6] = {
 	3 * 512,	/* 6MB */
 	2 * 1024,	/* 8MB */
 	4 * 1024,	/* 16MB */
@@ -69,94 +67,17 @@ static size_t lowmem_minfree[6] = {
 };
 static int lowmem_minfree_size = 4;
 
-static size_t lowmem_minfile[6] = {
-	1536,
-	2048,
-	4096,
-	7168,
-	9728,
-	12800
-};
-static int lowmem_minfile_size = 6;
-
-
-static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
-static uint32_t lowmem_check_filepages = 0;
-
-#ifdef CONFIG_SWAP
-static int fudgeswap = 512;
-#endif
-
 
 #define lowmem_print(level, x...)			\
 	do {						\
-		if (lowmem_debug_level >= (level)) {	\
-			printk("lowmem: ");		\
+		if (lowmem_debug_level >= (level))	\
 			printk(x);			\
-		}					\
 	} while (0)
-
-static int
-task_notify_func(struct notifier_block *self, unsigned long val, void *data);
-
-static struct notifier_block task_nb = {
-	.notifier_call	= task_notify_func,
-};
-
-static int
-task_notify_func(struct notifier_block *self, unsigned long val, void *data)
-{
-	struct task_struct *task = data;
-
-	if (task == lowmem_deathpending) {
-		lowmem_deathpending = NULL;
-		lowmem_print(2, "deathpending end %d (%s)\n",
-			task->pid, task->comm);
-	}
-
-	return NOTIFY_OK;
-}
-
-static void dump_deathpending(struct task_struct *t_deathpending)
-{
-	struct task_struct *p;
-
-	if (lowmem_debug_level < DEBUG_LEVEL_DEATHPENDING)
-		return;
-
-	BUG_ON(!t_deathpending);
-	lowmem_print(DEBUG_LEVEL_DEATHPENDING, "deathpending %d (%s)\n",
-		t_deathpending->pid, t_deathpending->comm);
-
-	read_lock(&tasklist_lock);
-	for_each_process(p) {
-		struct mm_struct *mm;
-		struct signal_struct *sig;
-		int oom_adj;
-		int tasksize;
-
-		task_lock(p);
-		mm = p->mm;
-		sig = p->signal;
-		if (!mm || !sig) {
-			task_unlock(p);
-			continue;
-		}
-		oom_adj = sig->oom_adj;
-		tasksize = get_mm_rss(mm);
-		task_unlock(p);
-		lowmem_print(DEBUG_LEVEL_DEATHPENDING,
-			"  %d (%s), adj %d, size %d\n",
-			p->pid, p->comm,
-			oom_adj, tasksize);
-	}
-	read_unlock(&tasklist_lock);
-}
 
 static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 {
-	struct task_struct *p;
+	struct task_struct *tsk;
 	struct task_struct *selected = NULL;
 	int rem = 0;
 	int tasksize;
@@ -165,55 +86,35 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 	int selected_tasksize = 0;
 	int selected_oom_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
-	int other_free = global_page_state(NR_FREE_PAGES);
+	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
+#ifdef SEC_ADJUST_LMK
+	int other_file = global_page_state(NR_INACTIVE_FILE) +
+						global_page_state(NR_ACTIVE_FILE);
+#else
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
-	int lru_file = global_page_state(NR_ACTIVE_FILE) +
-			global_page_state(NR_INACTIVE_FILE);
-
-	/*
-	 * If we already have a death outstanding, then
-	 * bail out right away; indicating to vmscan
-	 * that we have nothing further to offer on
-	 * this pass.
-	 *
-	 */
-	if (lowmem_deathpending &&
-	    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
-		dump_deathpending(lowmem_deathpending);
-		return 0;
-	}
-
-	#ifdef CONFIG_SWAP
-	if(fudgeswap != 0){
-		struct sysinfo si;
-		si_swapinfo(&si);
-
-		if(si.freeswap > 0){
-			if(fudgeswap > si.freeswap)
-				other_file += si.freeswap;
-			else
-				other_file += fudgeswap;
-		}
-	}
-	#endif
-
+#endif
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-               if (other_free < lowmem_minfree[i]) {
-                       if (other_file < lowmem_minfree[i] ||
-                               (lowmem_check_filepages &&
-                               (lru_file < lowmem_minfile[i]))) {
-                               min_adj = lowmem_adj[i];
-                               break;
-                       }
-
+#ifdef SEC_ADJUST_LMK
+		if ((other_free + other_file) < lowmem_minfree[i])
+#else
+		if (other_free < lowmem_minfree[i] &&
+            other_file < lowmem_minfree[i])
+#endif
+        {
+			min_adj = lowmem_adj[i];
+			break;
 		}
 	}
+#ifdef SEC_ADJUST_LMK
+	if (min_adj == OOM_ADJUST_MAX + 1)
+		return 0;
+#endif
 	if (nr_to_scan > 0)
 		lowmem_print(3, "lowmem_shrink %d, %x, ofree %d %d, ma %d\n",
 			     nr_to_scan, gfp_mask, other_free, other_file,
@@ -222,30 +123,43 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 		global_page_state(NR_ACTIVE_FILE) +
 		global_page_state(NR_INACTIVE_ANON) +
 		global_page_state(NR_INACTIVE_FILE);
-	if (nr_to_scan <= 0 || min_adj == OOM_ADJUST_MAX + 1) {
+#ifdef SEC_ADJUST_LMK
+	if (nr_to_scan <= 0)
+#else
+	if (nr_to_scan <= 0 || min_adj == OOM_ADJUST_MAX + 1)
+#endif
+	{
 		lowmem_print(5, "lowmem_shrink %d, %x, return %d\n",
 			     nr_to_scan, gfp_mask, rem);
 		return rem;
 	}
 	selected_oom_adj = min_adj;
 
-	read_lock(&tasklist_lock);
-	for_each_process(p) {
-		struct mm_struct *mm;
-
+	rcu_read_lock();
+	for_each_process(tsk) {
+		struct task_struct *p;
 		int oom_adj;
-		
-		if (p->flags & PF_KTHREAD)
-		continue;
 
-		task_lock(p);
-		mm = p->mm;
+		if (tsk->flags & PF_KTHREAD)
+			continue;
+
+		p = find_lock_task_mm(tsk);
+		if (!p)
+			continue;
+
+		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
+			time_before_eq(jiffies, lowmem_deathpending_timeout)) {
+				task_unlock(p);
+				rcu_read_unlock();
+				return 0;
+		}
+
 		oom_adj = p->signal->oom_adj;
 		if (oom_adj < min_adj) {
 			task_unlock(p);
 			continue;
 		}
-		tasksize = get_mm_rss(mm);
+		tasksize = get_mm_rss(p->mm);
 		task_unlock(p);
 		if (tasksize <= 0)
 			continue;
@@ -263,6 +177,7 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 			     p->pid, p->comm, oom_adj, tasksize);
 	}
 	if (selected) {
+		
 		if (fatal_signal_pending(selected)) {
 			pr_warning("process %d is suffering a slow death\n",
 				selected->pid);
@@ -273,14 +188,19 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 		lowmem_print(1, "send sigkill to %d (%s), adj %d, size %d\n",
 			     selected->pid, selected->comm,
 			     selected_oom_adj, selected_tasksize);
-		lowmem_deathpending = selected;
 		lowmem_deathpending_timeout = jiffies + HZ;
-		force_sig(SIGKILL, selected);
+		send_sig(SIGKILL, selected, 0);
+		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		rem -= selected_tasksize;
 	}
+#ifdef SEC_ADJUST_LMK
+	else
+		rem = -1;
+#endif
+
 	lowmem_print(4, "lowmem_shrink %d, %x, return %d\n",
 		     nr_to_scan, gfp_mask, rem);
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
 	return rem;
 }
 
@@ -291,18 +211,16 @@ static struct shrinker lowmem_shrinker = {
 
 static int __init lowmem_init(void)
 {
-	task_free_register(&task_nb);
 	register_shrinker(&lowmem_shrinker);
 	#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES 
 	init_kobject();
-	#endif
+	#endif	
 	return 0;
 }
 
 static void __exit lowmem_exit(void)
 {
 	unregister_shrinker(&lowmem_shrinker);
-	task_free_unregister(&task_nb);
 }
 
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES                                                                            
@@ -320,7 +238,7 @@ static void lowmem_autodetect_oom_adj_values(void)
        short oom_adj;                                                                                                                        
        short oom_score_adj;                                                                                                                  
        int array_size = ARRAY_SIZE(lowmem_adj);                                                                                              
-                                                                                                                                                     if (lowmem_adj_size < array_size)                                                                                                     
+       if (lowmem_adj_size < array_size)                                                                                                     
                array_size = lowmem_adj_size;                                                                                                 
                                                                                                                                              
        if (array_size <= 0)                                                                                                                  
@@ -363,14 +281,18 @@ static int lowmem_adj_array_get(char *buffer, struct kernel_param *kp)
 }
                                                                                                                                              
 static void lowmem_adj_array_free(void *arg)                                                                                                 
-{
-       kfree(arg);                                                                                                            
+{       
+	unsigned int i;
+	const struct kparam_array *arr = arg;
+ 
+	for (i = 0; i < (arr->num ? *arr->num : arr->max); i++)
+		kfree(arr->elem + arr->elemsize * i);
 }
                                                                                                                                              
                                                                                                                                              
 static const struct kparam_array __param_arr_adj = {
-       .max = ARRAY_SIZE(lowmem_adj),                                                                                                        
-       .num = &lowmem_adj_size,                                                                                                              
+       .max = ARRAY_SIZE(lowmem_adj),
+       .num = &lowmem_adj_size,
        .set = param_set_int,
        .get = param_get_int,
        .elemsize = sizeof(lowmem_adj[0]),                                                                                                    
@@ -419,36 +341,22 @@ static int init_kobject(void)
 }
 
 #endif                                                                                                                                       
-                                                                                                                                             
-                                                                                                                                            
 
 
 module_param_named(cost, lowmem_shrinker.seeks, int, S_IRUGO | S_IWUSR);
-
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES
 __module_param_call(MODULE_PARAM_PREFIX, adj,
 	lowmem_adj_array_set, lowmem_adj_array_get,
 	.arr = &__param_arr_adj,
 	S_IRUGO | S_IWUSR, 1);
-__MODULE_PARM_TYPE(adj, "array of int");
+	__MODULE_PARM_TYPE(adj, "array of int");
 #else
 module_param_array_named(adj, lowmem_adj, int, &lowmem_adj_size,
-			 S_IRUGO | S_IWUSR);
+	S_IRUGO | S_IWUSR);
 #endif
-
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
-
-module_param_named(check_filepages , lowmem_check_filepages, uint,
-		   S_IRUGO | S_IWUSR);
-module_param_array_named(minfile, lowmem_minfile, uint, &lowmem_minfile_size,
-			 S_IRUGO | S_IWUSR);
-
-#ifdef CONFIG_SWAP
-module_param_named(fudgeswap, fudgeswap, int, S_IRUGO | S_IWUSR);
-#endif
-
 
 module_init(lowmem_init);
 module_exit(lowmem_exit);
